67fcb8c8ac4002f574652767f1d79aa990f10188
Giovanni Remanente <gioremanente13@gmail.com>
2023-09-27 10:13:12 +0800

    reject request if user is not signed in

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import {
   Post,
   Query,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './DTOs/create-user.dto';
 import { UsersService } from './users.service';
@@ -19,6 +20,7 @@ import { UserDto } from './DTOs/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user-decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -28,6 +30,7 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  @UseGuards(AuthGuard)
   @Get('/currentUser')
   currentUser(@CurrentUser() user: User) {
     return user;
