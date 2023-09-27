d86b3e3832f9e867c248d3111ee992c1c408d576
Giovanni Remanente <gioremanente13@gmail.com>
2023-09-27 17:43:03 +0800

    user controller testing

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'hello@email.com',
+          password: 'password',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([
+          { id: 1, email, password: 'password' } as User,
+        ]);
+      },
+      // update: () => {},
+      // remove: (id: number) => {
+      //   return Promise.resolve({ id } as User);
+      // },
+    };
+    fakeAuthService = {
+      // signup: () => {},
+      // signin: () => {},
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        { provide: UsersService, useValue: fakeUsersService },
+        { provide: AuthService, useValue: fakeAuthService },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
