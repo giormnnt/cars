0bf6b9ebd2033a72caac96a6ac14aee6c8ab080f
Gio <gio13remanente@gmail.com>
2023-09-28 19:48:17 +0800

    testing controller methods

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -23,14 +24,14 @@ describe('UsersController', () => {
           { id: 1, email, password: 'password' } as User,
         ]);
       },
-      // update: () => {},
-      // remove: (id: number) => {
-      //   return Promise.resolve({ id } as User);
-      // },
     };
     fakeAuthService = {
-      // signup: () => {},
-      // signin: () => {},
+      signup: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -46,5 +47,43 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('find all users with the given email', async () => {
+    const users = await controller.findAllUsers('hello@email.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('hello@email.com');
+  });
+
+  it('find single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('throws an error if user is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('updates session object and returns users', async () => {
+    const session = { userId: -1 };
+    const user = await controller.signIn(
+      { email: 'hello@email.com', password: 'password' },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+  });
+
+  it('creates user', async () => {
+    const session = { userId: -1 };
+    const user = await controller.createUser(
+      { email: 'hello@email.com', password: 'password' } as User,
+      session,
+    );
+
+    expect(user.email).toBe('hello@email.com');
+    expect(session.userId).toEqual(1);
   });
 });
